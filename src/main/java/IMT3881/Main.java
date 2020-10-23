3cfc5587e3912af4c253549aa581e31252b79bac
bako1 <abdi@bako.noreply.github.com>
2020-10-23 20:43:06 +0200

    repainting

--- src/main/java/IMT3881/Main.java
+++ src/main/java/IMT3881/Main.java
@@ -1,11 +1,12 @@
 package IMT3881;
 
-import java.awt.*;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        EventQueue.invokeLater(() -> {
+        SwingUtilities.invokeLater(() -> {
+
             Interface inter = new Interface();
             inter.navigationBar();
         });
